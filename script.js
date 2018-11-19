4e2af6118328cdce8ad49a6da4739e27a6a071ff
“tetekitisgit config --global user.email “YOUR EMAIL ADDRESS”git config --global user.email “YOUR EMAIL ADDRESS”git config --global user.email “teteaficano@gmail.com <thomasnielene@gmail.com>
2018-11-19 16:19:48 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+o = ["th", "st", "nd", "rd"];
+
+for (var i = 0; i < 7 ; i++) {
+	console.log(o[i] += color[i])
+	//console.log("1" + o[i] "choice is " + color[i]);
+}
+
+
+
